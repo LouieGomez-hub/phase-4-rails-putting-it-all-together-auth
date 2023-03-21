b77567b4c6451ef92cb44c77708e30b28174165e
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-20 21:56:44 -0400

    recipe create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,22 @@ class RecipesController < ApplicationController
         end
     end
 
+    def create
+        recipe = Recipe.create(recipe_params)
+        if recipe.valid?
+            session[:recipe_id] = recipe.id
+            render json: recipe, status: :created
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :recipe_id
+        else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
 end
