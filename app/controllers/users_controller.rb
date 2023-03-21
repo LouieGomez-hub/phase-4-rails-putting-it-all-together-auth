adc64632da4c58dcef2de5f24155e03e18c59489
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-20 20:51:55 -0400

    user show route and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
 
     def create
         user = User.create(user_params)
@@ -10,9 +11,19 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        user = User.find_by(:id session[:user_id])
+        render json: user, status: :created
+    end
+
     private 
 
     def user_params
         params.permit(:username, :password, :password_confirmation, :image_url, :bio)
     end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
