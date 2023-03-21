c7d9b48731a18416257f54d7dfd3fe062081efc2
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-20 21:40:45 -0400

    recipe index route and action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+
+    def index
+        recipe = Recipe.all
+        if user.valid?
+            session[:user_id] = user.id
+            render json: recipe, status: :created
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+        end
+    end
+
 end
